b87a045538cd22e594e30366a82fd52b89a1628e
JafetEscobedo <jafu.escobedo@gmail.com>
2017-08-02 13:44:18 -0500

    Update conection.php

--- conection.php
+++ conection.php
@@ -9,22 +9,22 @@ class Conection
 	function __construct()
 	{
 		$user = "root";
-        $password = "";
-        $db_name = "paises";
-        $host = "localhost";
-        $conection_info = "mysql:host=$host;dbname=$db_name";
+		$password = "";
+		$db_name = "paises";
+		$host = "localhost";
+		$conection_info = "mysql:host=$host;dbname=$db_name";
 
-        try
-        {
-            $this->conection = new PDO($conection_info, $user, $password);
-            $this->conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->conection->exec("set character set utf8");
-        }
-        catch (Exception $ex)
-        {
-            echo "Ocurrió un error. Detalles: " . $ex->getMessage();
-            exit();
-        }
+		try
+		{
+		    $this->conection = new PDO($conection_info, $user, $password);
+		    $this->conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		    $this->conection->exec("set character set utf8");
+		}
+		catch (Exception $ex)
+		{
+		    echo "Ocurrió un error. Detalles: " . $ex->getMessage();
+		    exit();
+		}
 	}
 }
 ?>
