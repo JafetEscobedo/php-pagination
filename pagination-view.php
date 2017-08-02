2f5689c4ce3c27c37369efcd4ffb9e3a79210e32
JafetEscobedo <jafu.escobedo@gmail.com>
2017-08-02 13:48:54 -0500

    Update pagination-view.php

--- pagination-view.php
+++ pagination-view.php
@@ -29,34 +29,33 @@
 		    	<tr>
 			        <td><?php echo $row["id_estado"]; ?></td>
 			        <td><?php echo $row["estado"]; ?></td>
-					<td><?php echo $row["id_pais"]; ?></td>
+				<td><?php echo $row["id_pais"]; ?></td>
 		    	</tr>
-		    <?php endforeach; ?>					
+		    	<?php endforeach; ?>					
 			</tbody>				
 		</table>		
 
 		<nav>
 		  	<ul class="pagination">
 		  		<?php if ($data["actual-section"] != 1): ?> 		  			
-		    	<li><a href="pagination-view.php?page=1">Inicio</a></li>
-		    	<li><a href="pagination-view.php?page=<?php echo $data['previous']; ?>">&laquo;</a></li>
+		    		<li><a href="pagination-view.php?page=1">Inicio</a></li>
+		    		<li><a href="pagination-view.php?page=<?php echo $data['previous']; ?>">&laquo;</a></li>
 				<?php endif; ?>
 
 				<?php for ($i = $data["section-start"]; $i <= $data["section-end"]; $i++): ?>					
 				<?php if ($i > $data["total-pages"]): break; endif; ?>
-
 				<?php $active = ($i == $data["this-page"]) ? "active" : ""; ?>			    
-			    <li class="<?php echo $active; ?>">
-			    	<a href="pagination-view.php?page=<?php echo $i; ?>">
-			    		<?php echo $i; ?>			    		
-			    	</a>
-			    </li>
-			    <?php endfor; ?>
+			    	<li class="<?php echo $active; ?>">
+					<a href="pagination-view.php?page=<?php echo $i; ?>">
+						<?php echo $i; ?>			    		
+					</a>
+			    	</li>
+			    	<?php endfor; ?>
 				
 				<?php if ($data["actual-section"] != $data["total-sections"]): ?>
-			    <li><a href="pagination-view.php?page=<?php echo $data['next']; ?>">&raquo;</a></li>
-			    <li><a href="pagination-view.php?page=<?php echo $data['total-pages']; ?>">Final</a></li>
-			    <?php endif; ?>
+			    	<li><a href="pagination-view.php?page=<?php echo $data['next']; ?>">&raquo;</a></li>
+			    	<li><a href="pagination-view.php?page=<?php echo $data['total-pages']; ?>">Final</a></li>
+		    		<?php endif; ?>
 		  	</ul>
 		</nav>
 	</div>
